91d535f871e4877700594cbe801d111bbb6f6c01
AaronJT1 <tanjoelaaron@gmail.com>
2025-10-07 19:20:07 +0800

    Create UI tests for SearchInput

---
+++ tests/SearchInput.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test('should return correct search result for full keyword of a single valid product', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('searchbox', { name: 'Search' }).click();
+    await page.getByRole('searchbox', { name: 'Search' }).fill('Laptop');
+    await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Laptop' })).toBeVisible();
+    await expect(page.getByRole('img', { name: 'Laptop' })).toBeVisible();
+  });
+
+test('should return correct search result for partial keyword of a single valid product', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('searchbox', { name: 'Search' }).click();
+    await page.getByRole('searchbox', { name: 'Search' }).fill('No');
+    await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Novel' })).toBeVisible();
+    await expect(page.getByRole('img', { name: 'Novel' })).toBeVisible();
+});
+
+test('should return correct search result for partial keyword of multiple valid products', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('searchbox', { name: 'Search' }).click();
+    await page.getByRole('searchbox', { name: 'Search' }).fill('Best');
+    await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Novel' })).toBeVisible();
+    await expect(page.getByRole('img', { name: 'Novel' })).toBeVisible();
+    await expect(page.getByText('A bestselling novel...')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'The Law of Contract in' })).toBeVisible();
+    await expect(page.getByRole('img', { name: 'The Law of Contract in' })).toBeVisible();
+    await expect(page.getByText('A bestselling book in')).toBeVisible();
+
+});
+
+test('should return no products found for invalid product keyword', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('searchbox', { name: 'Search' }).click();
+    await page.getByRole('searchbox', { name: 'Search' }).fill('Random');
+    await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'No Products Found' })).toBeVisible();
+});
+
+
+
+
+
