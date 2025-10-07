5234e306395f702c73d73131d6caf279d71893a7
AaronJT1 <tanjoelaaron@gmail.com>
2025-10-07 21:01:01 +0800

    Improve UI test

--- tests/SearchInput.spec.ts
+++ tests/SearchInput.spec.ts
@@ -1,44 +1,62 @@
 import { test, expect } from '@playwright/test';
 
-test('should return correct search result for full keyword of a single valid product', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/');
+})
+
+test('should return correct search result for full keyword of a single valid product', async ({ page }) => {
     await page.getByRole('searchbox', { name: 'Search' }).click();
     await page.getByRole('searchbox', { name: 'Search' }).fill('Laptop');
     await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Found 1' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Laptop' })).toBeVisible();
     await expect(page.getByRole('img', { name: 'Laptop' })).toBeVisible();
   });
 
 test('should return correct search result for partial keyword of a single valid product', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
     await page.getByRole('searchbox', { name: 'Search' }).click();
     await page.getByRole('searchbox', { name: 'Search' }).fill('No');
     await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Found 1' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Novel' })).toBeVisible();
     await expect(page.getByRole('img', { name: 'Novel' })).toBeVisible();
 });
 
 test('should return correct search result for partial keyword of multiple valid products', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
     await page.getByRole('searchbox', { name: 'Search' }).click();
     await page.getByRole('searchbox', { name: 'Search' }).fill('Best');
     await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Found 2' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Novel' })).toBeVisible();
     await expect(page.getByRole('img', { name: 'Novel' })).toBeVisible();
     await expect(page.getByText('A bestselling novel...')).toBeVisible();
     await expect(page.getByRole('heading', { name: 'The Law of Contract in' })).toBeVisible();
     await expect(page.getByRole('img', { name: 'The Law of Contract in' })).toBeVisible();
     await expect(page.getByText('A bestselling book in')).toBeVisible();
-
 });
 
 test('should return no products found for invalid product keyword', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
     await page.getByRole('searchbox', { name: 'Search' }).click();
     await page.getByRole('searchbox', { name: 'Search' }).fill('Random');
     await page.getByRole('button', { name: 'Search' }).click();
     await expect(page.getByRole('heading', { name: 'No Products Found' })).toBeVisible();
 });
+
+test('should maintain search context', async ({ page }) => {
+    await page.getByRole('searchbox', { name: 'Search' }).click();
+    await page.getByRole('searchbox', { name: 'Search' }).fill('Laptop');
+    await page.getByRole('button', { name: 'Search' }).click();
+    await expect(page.getByRole('heading', { name: 'Found 1' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Laptop' })).toBeVisible();
+    await expect(page.getByRole('img', { name: 'Laptop' })).toBeVisible();
+
+    // go to a different component
+    await page.getByRole('button', { name: 'Cart' }).click();
+
+    // should still have the search context keyword
+    await expect(page.getByRole('searchbox', { name: 'Search' })).toHaveValue('Laptop');
+  });
+
 
 
 
