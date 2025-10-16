8a869f3c69b0ca4ec3216be444c7315b0ed66e39
AaronJT1 <tanjoelaaron@gmail.com>
2025-10-16 11:36:37 +0800

    Update homepage to use user context

--- tests/ui/pages/HomePage.spec.js
+++ tests/ui/pages/HomePage.spec.js
@@ -1,14 +1,21 @@
-import { test, expect } from "@playwright/test";
+import { test as base, expect } from "@playwright/test";
+
+
+// For normal user
+export const testUser = base.extend({
+    storageState: "tests/ui/.auth/user.json",
+});
+
 
 // Run tests in parallel to speed up execution
-test.describe.configure({ mode: "parallel" });
+testUser.describe.configure({ mode: "parallel" });
 
-test.describe("Home Page", () => {
-    test.beforeEach(async ({ page }) => {
+testUser.describe("Home Page", () => {
+    testUser.beforeEach(async ({ page }) => {
         await page.goto("/");
     })
 
-    test('should load all products successfully', async ({ page }) => {
+    testUser('should load all products successfully', async ({ page }) => {
         await expect(page.getByRole('heading', { name: 'All Products' })).toBeVisible();
         await expect(page.getByRole('article', { name: 'Novel'})).toBeVisible();
         await expect(page.getByRole('article', { name: 'The Law of Contract in Singapore'})).toBeVisible();
@@ -24,14 +31,14 @@ test.describe("Home Page", () => {
         }
     })
 
-    test('should load all categories successfully', async ({ page }) => {
+    testUser('should load all categories successfully', async ({ page }) => {
         await expect(page.getByRole('heading', { name: 'Filter By Category' })).toBeVisible();
         await expect(page.getByRole('checkbox', { name: 'Gadgets' })).toBeVisible();
         await expect(page.getByRole('checkbox', { name: 'Book' })).toBeVisible();
         await expect(page.getByRole('checkbox', { name: 'Clothing' })).toBeVisible();
     })
 
-    test('should load all prices successfully', async ({ page }) => {
+    testUser('should load all prices successfully', async ({ page }) => {
         await expect(page.getByRole('heading', { name: 'Filter By Price' })).toBeVisible();
         await expect(page.getByRole('radio', { name: '$0 to 19.99' })).toBeVisible();
         await expect(page.getByRole('radio', { name: '$20 to 39.99' })).toBeVisible();
@@ -41,37 +48,37 @@ test.describe("Home Page", () => {
         await expect(page.getByRole('radio', { name: '$100 or more' })).toBeVisible();
     })
 
-    test('should navigate to product details page when "More Details" is clicked', async ({ page }) => {
+    testUser('should navigate to product details page when "More Details" is clicked', async ({ page }) => {
         await page.getByRole('button', { name: 'More Details', exact: true }).first().click();
         await expect(page).toHaveURL(/\/product\/.+/);
         await expect(page.getByText('Product Details')).toBeVisible();
     })
 
-    test('should filter products by category', async ({ page }) => {
+    testUser('should filter products by category', async ({ page }) => {
         await page.getByRole('checkbox', { name: 'Gadgets' }).check();
         await expect(page.getByRole('article', { name: 'Smartphone'})).toBeVisible();
         await expect(page.getByRole('article', { name: 'Laptop'})).toBeVisible();
         const list = page.locator('[aria-label="product-list"]');
-        await expect(list.getByRole('article')).toHaveCount(2); 
+        await expect(list.getByRole('article')).toHaveCount(2);
     })
 
-    test('should filter products by price', async ({ page }) => {
+    testUser('should filter products by price', async ({ page }) => {
         await page.getByRole('radio', { name: '$0 to 19.99' }).check();
         await expect(page.getByRole('article', { name: 'Novel'})).toBeVisible();
         await expect(page.getByRole('article', { name: 'NUS T-shirt'})).toBeVisible();
         const list = page.locator('[aria-label="product-list"]');
-        await expect(list.getByRole('article')).toHaveCount(2); 
+        await expect(list.getByRole('article')).toHaveCount(2);
     })
 
-    test('should filter products by category and price', async ({ page }) => {
+    testUser('should filter products by category and price', async ({ page }) => {
         await page.getByRole('checkbox', { name: 'Book' }).check();
         await page.getByRole('radio', { name: '$0 to 19.99' }).check();
         await expect(page.getByRole('article', { name: 'Novel'})).toBeVisible();
         const list = page.locator('[aria-label="product-list"]');
-        await expect(list.getByRole('article')).toHaveCount(1); 
+        await expect(list.getByRole('article')).toHaveCount(1);
     })
 
-    test('should reset filter when "RESET FILTERS" button is clicked', async ({ page }) => {
+    testUser('should reset filter when "RESET FILTERS" button is clicked', async ({ page }) => {
         await page.getByRole('checkbox', { name: 'Book' }).check();
         await page.getByRole('radio', { name: '$0 to 19.99' }).check();
         await expect(page.getByRole('article', { name: 'Novel'})).toBeVisible();
@@ -82,7 +89,7 @@ test.describe("Home Page", () => {
         await expect(newlist.getByRole('article')).toHaveCount(6);
     })
 
-    test('should add product to cart when "ADD TO CART" button is clicked', async ({ page }) => {
+    testUser('should add product to cart when "ADD TO CART" button is clicked', async ({ page }) => {
         await page.getByRole('button', { name: 'ADD TO CART', exact: true }).first().click();
         await expect(page.getByText('Item Added to cart')).toBeVisible();
         const badge = page.locator('a[href="/cart"] + sup.ant-badge-count');
