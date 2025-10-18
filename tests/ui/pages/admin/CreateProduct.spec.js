18bc29af61f1aedea11baa3f1cada30b454e54e6
lamchenghou <lamchenghouu@gmail.com>
2025-10-18 13:13:39 +0800

    tests: add remaining e2e ui tests for CreateProduct

--- tests/ui/pages/admin/CreateProduct.spec.js
+++ tests/ui/pages/admin/CreateProduct.spec.js
@@ -9,7 +9,6 @@ export const testAdmin = base.extend({
 testAdmin.describe.configure({ mode: "parallel" });
 
 testAdmin.describe("Create Product Page", () => {
-
   let context;
   let page;
 
@@ -21,77 +20,188 @@ testAdmin.describe("Create Product Page", () => {
   testAdmin.afterAll(async () => {
     // Clean up - delete the product created so it doesn't affect future tests
     await page.goto("/dashboard/admin/product/Nature-Book");
-    page.on('dialog', async dialog => {
-      await dialog.accept('yes');
+    page.on("dialog", async (dialog) => {
+      await dialog.accept("yes");
     });
     await page.getByRole("button", { name: "DELETE PRODUCT" }).click();
     await page.goto("/dashboard/admin/products");
     await expect(page.getByText("Nature Book")).not.toBeVisible();
   });
 
-  testAdmin("should allow me to create a product when all fields filled in", async ({
-    page,
-  }) => {
-    // Arrange + Act
-    await page.goto("/dashboard/admin/create-product");
+  testAdmin(
+    "should allow me to create a product when all fields filled in",
+    async ({ page }) => {
+      // Arrange + Act
+      await page.goto("/dashboard/admin/create-product");
 
-    // Fill in the form and assert that form values are updated correctly
-    await page
-      .locator("div")
-      .filter({ hasText: /^Select a category$/ })
-      .first()
-      .click();
-    await page.getByTitle("Book").locator("div").click();
-    await expect(page.getByRole("main")).toContainText("Book");
-
-    await page.getByText("Upload Photo").click();
-    await page
-      .getByLabel("Upload Photo")
-      .setInputFiles("tests/fixtures/test-photo.jpg");
-    // Preview shows up
-    await expect(
-      page.getByRole("img", { name: "product_photo" })
-    ).toBeVisible();
-
-    await page.getByRole("textbox", { name: "write a name" }).click();
-    await page
-      .getByRole("textbox", { name: "write a name" })
-      .fill("Nature Book");
-
-    await page.getByRole("textbox", { name: "write a description" }).click();
-    await page
-      .getByRole("textbox", { name: "write a description" })
-      .fill("Book on Nature");
-    await expect(page.getByPlaceholder("write a description")).toContainText(
-      "Book on Nature"
-    );
-
-    await page.getByPlaceholder("write a Price").click();
-    await page.getByPlaceholder("write a Price").fill("20");
-    await expect(page.getByPlaceholder("write a Price")).toHaveValue("20");
-
-    await page.getByPlaceholder("write a quantity").click();
-    await page.getByPlaceholder("write a quantity").fill("20");
-    await expect(page.getByPlaceholder("write a quantity")).toHaveValue("20");
-
-    await page.locator(".mb-3 > .ant-select").click();
-    await page.getByText("Yes").click();
-    await expect(page.getByRole("main")).toContainText("Yes");
-
-    await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
-
-    // Assert that the product was created successfully
-    await page.waitForURL("dashboard/admin/products");
-    // Success toast shows up
-    await expect(
-      page
+      // Fill in the form and assert that form values are updated correctly
+      await page
         .locator("div")
-        .filter({ hasText: /^Product Created Successfully$/ })
-        .nth(2)
-    ).toBeVisible();
+        .filter({ hasText: /^Select a category$/ })
+        .first()
+        .click();
+      await page.getByTitle("Book").locator("div").click();
+      await expect(page.getByRole("main")).toContainText("Book");
 
-    // Navigated to products page and product shows up in the list
-    await expect(page.getByRole("main")).toContainText("Nature Book");
-    await expect(page.getByRole("main")).toContainText("Book on Nature");
-  });
+      await page.getByText("Upload Photo").click();
+      await page
+        .getByLabel("Upload Photo")
+        .setInputFiles("tests/fixtures/test-photo.jpg");
+      // Preview shows up
+      await expect(
+        page.getByRole("img", { name: "product_photo" })
+      ).toBeVisible();
+
+      await page.getByRole("textbox", { name: "write a name" }).click();
+      await page
+        .getByRole("textbox", { name: "write a name" })
+        .fill("Nature Book");
+
+      await page.getByRole("textbox", { name: "write a description" }).click();
+      await page
+        .getByRole("textbox", { name: "write a description" })
+        .fill("Book on Nature");
+      await expect(page.getByPlaceholder("write a description")).toContainText(
+        "Book on Nature"
+      );
+
+      await page.getByPlaceholder("write a Price").click();
+      await page.getByPlaceholder("write a Price").fill("20");
+      await expect(page.getByPlaceholder("write a Price")).toHaveValue("20");
+
+      await page.getByPlaceholder("write a quantity").click();
+      await page.getByPlaceholder("write a quantity").fill("20");
+      await expect(page.getByPlaceholder("write a quantity")).toHaveValue("20");
+
+      await page.locator(".mb-3 > .ant-select").click();
+      await page.getByText("Yes").click();
+      await expect(page.getByRole("main")).toContainText("Yes");
+
+      await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+
+      // Assert that the product was created successfully
+      await page.waitForURL("dashboard/admin/products");
+      // Success toast shows up
+      await expect(
+        page
+          .locator("div")
+          .filter({ hasText: /^Product Created Successfully$/ })
+          .nth(2)
+      ).toBeVisible();
+
+      // Navigated to products page and product shows up in the list
+      await expect(page.getByRole("main")).toContainText("Nature Book");
+      await expect(page.getByRole("main")).toContainText("Book on Nature");
+    }
+  );
+
+  testAdmin(
+    "should not allow me to create a product when product name field not filled in",
+    async ({ page }) => {
+      // Arrange + Act
+      await page.goto("/dashboard/admin/create-product");
+
+      // Fill in the form and assert that form values are updated correctly
+      await page
+        .locator("div")
+        .filter({ hasText: /^Select a category$/ })
+        .first()
+        .click();
+      await page.getByTitle("Book").locator("div").click();
+      await expect(page.getByRole("main")).toContainText("Book");
+
+      await page.getByText("Upload Photo").click();
+      await page
+        .getByLabel("Upload Photo")
+        .setInputFiles("tests/fixtures/test-photo.jpg");
+      // Preview shows up
+      await expect(
+        page.getByRole("img", { name: "product_photo" })
+      ).toBeVisible();
+
+      await page.getByRole("textbox", { name: "write a description" }).click();
+      await page
+        .getByRole("textbox", { name: "write a description" })
+        .fill("Book on Nature");
+      await expect(page.getByPlaceholder("write a description")).toContainText(
+        "Book on Nature"
+      );
+
+      await page.getByPlaceholder("write a Price").click();
+      await page.getByPlaceholder("write a Price").fill("20");
+      await expect(page.getByPlaceholder("write a Price")).toHaveValue("20");
+
+      await page.getByPlaceholder("write a quantity").click();
+      await page.getByPlaceholder("write a quantity").fill("20");
+      await expect(page.getByPlaceholder("write a quantity")).toHaveValue("20");
+
+      await page.locator(".mb-3 > .ant-select").click();
+      await page.getByText("Yes").click();
+      await expect(page.getByRole("main")).toContainText("Yes");
+
+      await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+
+      // Assert - Error toast shows up
+      await expect(
+        page
+          .locator("div")
+          .filter({ hasText: /^Name is Required$/ })
+          .nth(2)
+      ).toBeVisible();
+    }
+  );
+
+  testAdmin(
+    "should not allow me to create a product when product photo is not uploaded",
+    async ({ page }) => {
+      // Arrange + Act
+      await page.goto("/dashboard/admin/create-product");
+
+      // Fill in the form and assert that form values are updated correctly
+      await page
+        .locator("div")
+        .filter({ hasText: /^Select a category$/ })
+        .first()
+        .click();
+      await page.getByTitle("Book").locator("div").click();
+      await expect(page.getByRole("main")).toContainText("Book");
+
+      await page.getByRole("textbox", { name: "write a name" }).click();
+      await page
+        .getByRole("textbox", { name: "write a name" })
+        .fill("Nature Book");
+
+      await page.getByRole("textbox", { name: "write a description" }).click();
+      await page
+        .getByRole("textbox", { name: "write a description" })
+        .fill("Book on Nature");
+      await expect(page.getByPlaceholder("write a description")).toContainText(
+        "Book on Nature"
+      );
+
+      await page.getByPlaceholder("write a Price").click();
+      await page.getByPlaceholder("write a Price").fill("20");
+      await expect(page.getByPlaceholder("write a Price")).toHaveValue("20");
+
+      await page.getByPlaceholder("write a quantity").click();
+      await page.getByPlaceholder("write a quantity").fill("20");
+      await expect(page.getByPlaceholder("write a quantity")).toHaveValue("20");
+
+      await page.locator(".mb-3 > .ant-select").click();
+      await page.getByText("Yes").click();
+      await expect(page.getByRole("main")).toContainText("Yes");
+
+      await page.getByRole("button", { name: "CREATE PRODUCT" }).click();
+
+      // Assert - Error toast shows up
+      await expect(
+        page
+          .locator("div")
+          .filter({
+            hasText: /^Photo is Required and should be less than 1mb$/,
+          })
+          .nth(2)
+      ).toBeVisible();
+    }
+  );
 });
