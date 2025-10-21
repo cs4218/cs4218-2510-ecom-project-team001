608220cf96e47b4316c2cb9e3a44aa56bdeb2ac3
jingting1412 <jingting.sg@gmail.com>
2025-10-21 12:14:03 +0800

    Improve Categories integration tests

--- client/src/integration-tests/Categories.integration.test.js
+++ client/src/integration-tests/Categories.integration.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import axios from 'axios';
 import app from '../../../server.js';
 import categoryModel from '../../../models/categoryModel';
@@ -15,12 +15,6 @@ import { connectToTestDb, resetTestDb, disconnectFromTestDb } from '../../../tes
  * with the /categories page
  * 
  */
-
-const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockNavigate,
-}));
 
 jest.mock('../components/Layout', () => {
   return function MockLayout({ children }) {
@@ -47,7 +41,6 @@ describe('Categories Integration Tests', () => {
 
   beforeEach(async () => {
     localStorage.clear();
-    mockNavigate.mockClear();
     await resetTestDb();
     
     const PORT = 8081;
@@ -85,26 +78,14 @@ describe('Categories Integration Tests', () => {
 
     expect(await screen.findByText("Cat 1")).toBeInTheDocument();
     expect(await screen.findByText("Cat 2")).toBeInTheDocument();
-    expect(await screen.findByRole('link', { name: '/category/cat1' })).toBeInTheDocument();
-    expect(await screen.findByRole('link', { name: '/category/cat2' })).toBeInTheDocument();
-  });
+    expect(await screen.findByRole('link', { name: 'Cat 1' })).toBeInTheDocument();
+    expect(await screen.findByRole('link', { name: 'Cat 2' })).toBeInTheDocument();
 
-  test('should naviagte to category page on category link click', async () => {
-    renderCategories();
+    const cat1 = await screen.findByRole('link', { name: 'Cat 1' });
+    expect(cat1).toHaveAttribute('href', '/category/cat1');
 
-    const cat1Link = await screen.findByRole('link', { name: 'Cat 1' });
-    fireEvent.click(cat1Link);
-
-    await waitFor(() => {
-      expect(mockNavigate).toHaveBeenCalledWith('/category/cat1');
-    });
-
-    const cat2Link = await screen.findByRole('link', { name: 'Cat 2' });
-    fireEvent.click(cat2Link);
-
-    await waitFor(() => {
-      expect(mockNavigate).toHaveBeenCalledWith('/category/cat2');
-    });
+    const cat2 = await screen.findByRole('link', { name: 'Cat 2' });
+    expect(cat2).toHaveAttribute('href', '/category/cat2');
   });
 
   test('should display no categories found message when there are no categories', async () => {
